b4f89a1bb0582414b53124e14549c5aa9cf0ed65
lovbackan <simon@lovbacka.se>
2023-04-19 16:21:48 +0200

    Added new artist for Api and guess, added script that rerenders the picture if its broken and added a function where u dont get repeated paintings

--- src/components/ImageFinder/ImageFinder.js
+++ src/components/ImageFinder/ImageFinder.js
@@ -3,11 +3,21 @@ import ArtistGuesser from "../ArtistGuesser/ArtistGuesser";
 
 function ImageFinder() {
   const [data, setData] = useState([]);
+  const [showedPaintings, setShowedPaintings] = useState([]);
 
   const artistsApis = [
     "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Vincent%20Van%20Gogh",
     "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Goya",
     "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Henri%20Rousseau",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Thomas%20Cole",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Giovanni%20Bellini",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Andrea%20del%20Sarto",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Gustave%20delCourbet",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=James%20Hamilton",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Asher%20Brown%20Durand",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=James%20Mqneill%20Whistler",
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&medium=Paintings&artistOrCulture=true&q=Nicolas%20Poussin",
+    ,
   ];
 
   function getRandomArtist() {
@@ -33,10 +43,21 @@ function ImageFinder() {
       .then((randomObjectId) => {
         return fetch(
           `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomObjectId}`
-        );
+        ).then((response) => response.json());
       })
-      .then((response) => response.json())
-      .then((jsonData) => setData(jsonData))
+      .then((jsonData) => {
+        if (
+          jsonData.message === "Not a valid object" ||
+          jsonData.message === "ObjectID not found" ||
+          jsonData.primaryImage === "" ||
+          showedPaintings.includes(jsonData.objectID)
+        ) {
+          fetchData();
+        } else {
+          setData(jsonData);
+          setShowedPaintings((prevState) => [...prevState, jsonData.objectID]);
+        }
+      })
       .catch((error) => console.error(error))
       .finally(() => console.log("done"));
   }
