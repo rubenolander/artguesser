3c14ff2120d4badf53eb8d42bb80cca5bb4879bd
lovbackan <simon@lovbacka.se>
2023-04-19 12:53:39 +0200

    Now we have the right answer object in artistguesser!

--- src/components/ArtistGuesser/ArtistGuesser.js
+++ src/components/ArtistGuesser/ArtistGuesser.js
@@ -27,6 +27,18 @@ const ArtistGuesser = (props) => {
     },
   ];
 
+  let correctArtistId = 1000;
+  artistData.forEach((artist) => {
+    if (props.correctAnswer === artist.artistName) {
+      correctArtistId = artist.id;
+      return correctArtistId;
+    }
+  });
+
+  console.log(correctArtistId);
+  console.log(artistData[correctArtistId]);
+
+  //måste det verkligen vara en map här? Kan man inte ta den ordinarie arrayen och skapa en option för varje objekt i arrayen?
   const options = artistData.map((artist) => (
     <option key={artist.id} value={artist.id}>
       {artist.artistName}
