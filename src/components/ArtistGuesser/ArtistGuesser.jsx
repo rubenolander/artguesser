f352e9d8a099891145d3f5fb60d6f199b15abb0d
lovbackan <simon@lovbacka.se>
2023-04-25 13:58:46 +0200

    we might need to fix this code because its very messy but it works now

--- src/components/ArtistGuesser/ArtistGuesser.jsx
+++ src/components/ArtistGuesser/ArtistGuesser.jsx
@@ -63,6 +63,7 @@ const ArtistGuesser = (props) => {
       era: "Realism",
       born: 1808,
       died: 1870,
+      nationality: "Irish",
     },
     {
       id: 8,
@@ -70,6 +71,7 @@ const ArtistGuesser = (props) => {
       era: "Romanticism",
       born: 1796,
       died: 1886,
+      nationality: "American",
     },
     {
       id: 9,
@@ -77,6 +79,7 @@ const ArtistGuesser = (props) => {
       era: "Impressionism",
       born: 1834,
       died: 1903,
+      nationality: "American",
     },
     {
       id: 10,
@@ -84,6 +87,7 @@ const ArtistGuesser = (props) => {
       era: "Baroque",
       born: 1594,
       died: 1665,
+      nationality: "French",
     },
   ];
 
@@ -109,23 +113,48 @@ const ArtistGuesser = (props) => {
     const guess = document.querySelector("select").value;
     const artistGuessData = artistData[guess];
     const artistGuess = artistData[guess].artistName;
-    const guessGridItems = document.createElement('section');
-    guessGridItems.classList.add('gridGuessItems');
-    const gridContainer = document.querySelector('.gridContainer');
+    const guessGridItems = document.createElement("section");
+    guessGridItems.classList.add("gridGuessItems");
+    const gridContainer = document.querySelector(".gridContainer");
     gridContainer.append(guessGridItems);
-// skapa divar i en grid med datan från en gubbe
-    guesses ++;
+    // skapa divar i en grid med datan från en gubbe
+    guesses++;
     console.log(artistGuess);
     if (artistGuess === props.correctAnswer) {
-      alert("Correct!");
+      for (const key in artistGuessData) {
+        if (key === "id" || key === "artistName") {
+          continue;
+        } else {
+          createGridItemRight(guessGridItems, artistGuessData[key]);
+        }
+      }
+
       // måla divarna och sätt pilar beroende på om svaret är för högt/lågt/nära
     } else {
       for (const key in artistGuessData) {
-        if (key === "id" || key === "artistName"){continue}
-        else if (artistGuessData[key] === artistData[correctArtistId][key]) {
-          createGridItemRight(guessGridItems);
+        if (key === "id" || key === "artistName") {
+          continue;
+        } else if (artistGuessData[key] === artistData[correctArtistId][key]) {
+          // createGridItemRight(guessGridItems);
+          createGridItemRight(
+            guessGridItems,
+            artistGuessData[key],
+            artistData[correctArtistId][key]
+          );
+          console.log(artistGuessData[key], artistData[correctArtistId][key]);
         } else {
-          createGridItemWrong(guessGridItems);
+          // createGridItemWrong(guessGridItems);
+          createGridItemWrong(
+            guessGridItems,
+            artistGuessData[key],
+            artistData[correctArtistId][key]
+          );
+          console.log(
+            "Guess " +
+              artistGuessData[key] +
+              " Right answer " +
+              artistData[correctArtistId][key]
+          );
         }
       }
     }
@@ -133,15 +162,32 @@ const ArtistGuesser = (props) => {
 
   //skapar en röd ruta nu när man gissar fel. ska skapa en grid med röda rutor
 
-  function createGridItemWrong(guessGridItems) {
+  function createGridItemWrong(guessGridItems, key, correctAnswer) {
     const gridItemWrong = document.createElement("div");
     gridItemWrong.className = "gridItemWrong";
+    const pTag = document.createElement("p");
+    console.log(key);
+
+    if (typeof key === "number") {
+      if (key > correctAnswer) {
+        pTag.textContent = key + "v";
+      } else if (key < correctAnswer) {
+        pTag.textContent = key + "^";
+      }
+    } else {
+      pTag.textContent = key;
+    }
+
+    gridItemWrong.appendChild(pTag);
     guessGridItems.appendChild(gridItemWrong);
   }
 
-  function createGridItemRight(guessGridItems) {
+  function createGridItemRight(guessGridItems, key) {
     const gridItemRight = document.createElement("div");
     gridItemRight.className = "gridItemRight";
+    const pTag = document.createElement("p");
+    pTag.textContent = key;
+    gridItemRight.appendChild(pTag);
     guessGridItems.appendChild(gridItemRight);
   }
 
@@ -154,8 +200,8 @@ const ArtistGuesser = (props) => {
         {options}
       </select>
       <button onClick={guess}>Guess</button>
-      <section className="gridContainer">
-      </section>
+      <h2>Era, birthyear, date of death, nationality</h2>
+      <section className="gridContainer"></section>
       <h2>Answer: {props.correctAnswer}</h2>
     </section>
   );
